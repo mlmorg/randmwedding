b3fed79030898b79391ef0fdea36d01ca2ca598c
Matt Morgan <matt@mlmorg.com>
2016-07-05 21:07:01 -0700

    Refactor sheet submission

--- src/server/api.js
+++ src/server/api.js
@@ -1,21 +1,12 @@
-import google from 'googleapis';
-import GoogleAuth from 'google-auth-library';
-
-const creds = {
-  client_email: process.env.GOOGLE_CLIENT_EMAIL,
-  private_key: process.env.GOOGLE_PRIVATE_KEY
-};
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+import Sheets from './sheets';
 
 export default class API {
   constructor() {
-    this.auth = new GoogleAuth();
-    this.jwt = new this.auth.JWT(creds.client_email, null, creds.private_key, SCOPES, null);
-    this.sheets = google.sheets('v4');
+    this.sheets = new Sheets();
   }
 
   init(cb) {
-    this.jwt.authorize(cb);
+    cb(null);
   }
 
   saveAddress(req, body, cb) {
@@ -25,36 +16,16 @@ export default class API {
       return cb(err);
     }
 
-    this.sheets.spreadsheets.values.get({
-      auth: this.jwt,
-      spreadsheetId: '1LGd9S0eOtyVRYJnJnViTq60vZMiOesLmkiXyRTyQAek',
-      range: 'addresses!A1:C'
-    }, (getErr, getRes) => {
-      if (getErr) {
-        return cb(getErr);
+    const data = [
+      body.name,
+      body.street,
+      body.city
+    ];
+    this.sheets.addRow(data, (err) => {
+      if (err) {
+        return cb(err);
       }
-
-      const num = getRes.values.length + 1;
-      this.sheets.spreadsheets.values.update({
-        auth: this.jwt,
-        spreadsheetId: '1LGd9S0eOtyVRYJnJnViTq60vZMiOesLmkiXyRTyQAek',
-        range: `addresses!A${num}:C${num}`,
-        valueInputOption: 'RAW',
-        resource: {
-          range: `addresses!A${num}:C${num}`,
-          majorDimension: 'ROWS',
-          values: [[
-            body.name,
-            body.street,
-            body.city
-          ]]
-        }
-      }, (updateErr, updateRes) => {
-        if (updateErr) {
-          return cb(updateErr);
-        }
-        cb(null, {success: 'Address saved!'});
-      })
+      cb(null, {success: 'Address saved!'});
     });
   }
 }
---
+++ src/server/sheets.js
@@ -0,0 +1,72 @@
+import google from 'googleapis';
+import GoogleAuth from 'google-auth-library';
+
+const CREDS = {
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  private_key: process.env.GOOGLE_PRIVATE_KEY
+};
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const SPREADSHEET = '1LGd9S0eOtyVRYJnJnViTq60vZMiOesLmkiXyRTyQAek';
+
+export default class Sheets {
+  constructor() {
+    this.expiry = 0;
+    this.auth = new GoogleAuth();
+    this.jwt = new this.auth.JWT(CREDS.client_email, null, CREDS.private_key, SCOPES, null);
+    this.sheets = google.sheets('v4');
+    this.options = {
+      auth: this.jwt,
+      spreadsheetId: SPREADSHEET,
+      range: 'addresses!A1:C'
+    };
+  }
+
+  renew(cb) {
+    if (this.expiry > +new Date()) {
+      console.log('reused token');
+      return cb(null);
+    }
+
+    this.jwt.authorize((err, token) => {
+      if (err) {
+        return cb(err);
+      }
+
+      this.expiry = token.expiry_date;
+      console.log('expiry', this.expiry);
+      cb(null);
+    });
+  }
+
+  getValues(cb) {
+    this.renew((err) => {
+      if (err) {
+        return cb(err);
+      }
+
+      this.sheets.spreadsheets.values.get(this.options, cb);
+    });
+  }
+
+  addRow(data, cb) {
+    this.getValues((err, res) => {
+      if (err) {
+        return cb(err);
+      }
+
+      const nextRow = res.values.length + 1;
+      const options = {
+        ...this.options,
+        range: `addresses!A${nextRow}:C${nextRow}`,
+        valueInputOption: 'RAW',
+        resource: {
+          range: `addresses!A${nextRow}:C${nextRow}`,
+          majorDimension: 'ROWS',
+          values: [data]
+        }
+      };
+
+      this.sheets.spreadsheets.values.update(options, cb);
+    });
+  }
+}
